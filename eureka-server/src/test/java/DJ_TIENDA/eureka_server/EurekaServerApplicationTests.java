c387d6f821140c2ec1d909ac69357bbfc35d8f1c
Joaquin Rojas <joaq.rojasl@duocuc.cl>
2026-04-23 19:21:22 -0400

    Add files via upload

---
+++ eureka-server/src/test/java/DJ_TIENDA/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package DJ_TIENDA.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
