1e30f15f1b74f38d51f44ec889b7b3b73f6c1efc
Joaquin Rojas <joaq.rojasl@duocuc.cl>
2026-04-27 12:38:12 -0400

    Add files via upload

---
+++ api.gateway/src/main/java/DJ_TIENDA/api/gateway/Application.java
@@ -0,0 +1,13 @@
+package DJ_TIENDA.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
