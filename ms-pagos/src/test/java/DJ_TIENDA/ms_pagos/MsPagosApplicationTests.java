936fcf6d14e70f832f3576fbde4119a9da85f6a5
Diego Bastián Rivera Huenten <di.riverah@duocuc.cl>
2026-04-27 12:52:00 -0400

    Add files via upload

---
+++ ms-pagos/src/test/java/DJ_TIENDA/ms_pagos/MsPagosApplicationTests.java
@@ -0,0 +1,13 @@
+package DJ_TIENDA.ms_pagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
