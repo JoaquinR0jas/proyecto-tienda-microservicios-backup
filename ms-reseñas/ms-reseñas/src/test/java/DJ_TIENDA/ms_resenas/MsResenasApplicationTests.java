c2716109e8afade09b08dcceee38569362262f59
Diego Bastián Rivera Huenten <di.riverah@duocuc.cl>
2026-04-27 12:44:02 -0400

    Add files via upload

---
+++ ms-reseñas/ms-reseñas/src/test/java/DJ_TIENDA/ms_resenas/MsResenasApplicationTests.java
@@ -0,0 +1,13 @@
+package DJ_TIENDA.ms_resenas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsResenasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
