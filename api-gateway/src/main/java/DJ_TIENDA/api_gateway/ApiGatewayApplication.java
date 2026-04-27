e477ace1a7cb3f801f7a8048b22d44dbc11f99e1
Joaquin Rojas <joaq.rojasl@duocuc.cl>
2026-04-27 12:40:48 -0400

    ó
    k

--- api-gateway/src/main/java/DJ_TIENDA/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/DJ_TIENDA/api_gateway/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package DJ_TIENDA.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient // Permite que el Gateway "vea" a los microservicios en Eureka
