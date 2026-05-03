690c90beaac008c6a6bbc2e61319b8b7f0091d69
Joaquin Rojas <joaq.rojasl@duocuc.cl>
2026-05-03 13:30:37 -0400

    Apuntes

--- ms-catalogo/src/main/java/DJ/TIENDA/ms_catalogo/model/Producto.java
+++ ms-catalogo/src/main/java/DJ/TIENDA/ms_catalogo/model/Producto.java
@@ -7,15 +7,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-@Entity
-@Table(name = "productos")
-@Data
+@Entity // Indica a Spring que esta clase representa una tabla en la base de datos.
+@Table(name = "productos") // Define el nombre exacto que tendrá la tabla en MySQL.
+@Data // Magia de Lombok: Genera automáticamente Getters, Setters y constructores por detrás.
 public class Producto {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // Marca este campo como la Llave Primaria (Primary Key).
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Hace que el ID sea Autoincrementable en MySQL (1, 2, 3...).
     private Long id;
     
+    // Estos atributos se convertirán en columnas dentro de la tabla "productos"
     private String nombre;
     private String descripcion;
     private Double precio;
