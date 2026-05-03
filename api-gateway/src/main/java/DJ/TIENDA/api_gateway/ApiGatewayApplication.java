cbe6c5aa3a9e5a0992fb9ccb44449d5499c20d1a
Joaquin Rojas <joaq.rojasl@duocuc.cl>
2026-05-02 20:15:07 -0400

    z
    z

--- api-gateway/src/main/java/DJ/TIENDA/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/DJ/TIENDA/api_gateway/ApiGatewayApplication.java
@@ -2,7 +2,8 @@ package DJ.TIENDA.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
--- ms-catalogo/src/main/java/DJ/TIENDA/ms_catalogo/MsCatalogoApplication.java
+++ ms-catalogo/src/main/java/DJ/TIENDA/ms_catalogo/MsCatalogoApplication.java
@@ -2,7 +2,8 @@ package DJ.TIENDA.ms_catalogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsCatalogoApplication {
 
