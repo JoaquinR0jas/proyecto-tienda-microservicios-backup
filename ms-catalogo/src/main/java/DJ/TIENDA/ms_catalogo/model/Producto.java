27fecaf8c3f6c5d1978be9191d79244f04263096
Joaquin Rojas <joaq.rojasl@duocuc.cl>
2026-05-03 13:28:05 -0400

    creado

--- ms-catalogo/src/main/java/DJ/TIENDA/ms_catalogo/model/Producto.java
+++ ms-catalogo/src/main/java/DJ/TIENDA/ms_catalogo/model/Producto.java
@@ -1,5 +1,22 @@
 package DJ.TIENDA.ms_catalogo.model;
 
-public class Producto {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity
+@Table(name = "productos")
+@Data
+public class Producto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nombre;
+    private String descripcion;
+    private Double precio;
 }
