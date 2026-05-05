cb186e69d4a81ff82fbd6704fd947a89e450c3c2
Joaquin Rojas <joaq.rojasl@duocuc.cl>
2026-05-04 21:42:37 -0400

    s

--- auth-service/src/main/java/DJ/TIENDA/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/DJ/TIENDA/auth_service/AuthServiceApplication.java
@@ -1,13 +1,16 @@
 package DJ.TIENDA.auth_service;
+package DJ.TIENDA.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Se registra en Eureka
+@EnableFeignClients    // Habilita Feign para consultar ms-usuarios
 public class AuthServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
 }
